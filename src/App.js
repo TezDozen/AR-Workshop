5f96d227735e47a2558c146335553104a2d3ec12
Hsiang <sam880222@gmail.com>
2022-10-26 20:18:42 +0800

    Fix animation not working problem.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "mind-ar/dist/mindar-image.prod.js";
 import "aframe";
 import "mind-ar/dist/mindar-image-aframe.prod.js";
+import "aframe-extras/dist/aframe-extras.loaders";
 import "./App.css";
 import MindARViewer from "./mindar-viewer";
-import "aframe-extras";
 
 function App() {
   return (
--- src/mindar-viewer.js
+++ src/mindar-viewer.js
@@ -1,48 +1,30 @@
-import React, { useEffect, useRef } from "react";
-import "aframe-extras";
-
 const MindARViewer = () => {
-  const sceneRef = useRef(null);
-
-  useEffect(() => {
-    const sceneEl = sceneRef.current;
-    const arSystem = sceneEl.systems["mindar-image-system"];
-    sceneEl.addEventListener("renderstart", () => {
-      arSystem.start(); // start AR
-    });
-    return () => {
-      arSystem.stop();
-    };
-  }, []);
-
   return (
     <a-scene
-      ref={sceneRef}
-      mindar-image="imageTargetSrc: /targets.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+      mindar-image="imageTargetSrc: /mind-ar-test/targets.mind; maxTrack: 1"
+      //   mindar-image="imageTargetSrc: /mind-ar-test/targets.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+      //   embedded
       color-space="sRGB"
-      embedded
       renderer="colorManagement: true, physicallyCorrectLights"
       vr-mode-ui="enabled: false"
       device-orientation-permission-ui="enabled: false"
     >
       <a-assets>
-        <img
-          id="card"
-          alt="target"
-          src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/card.png"
-        />
-        <a-asset-item id="avatarModel" src="/model.glb"></a-asset-item>
+        <a-asset-item id="model" src="/mind-ar-test/model2.glb"></a-asset-item>
       </a-assets>
 
       <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-
       <a-entity mindar-image-target="targetIndex: 0">
         <a-gltf-model
           rotation="0 0 0 "
           position="0 0 0"
           scale="0.5 0.5 0.5"
-          src="#avatarModel"
-          animation-mixer
+          src="#model"
+          /**
+           * IMPORTANT!!!
+           * If add this component without value, React will give it true instead of the default value!
+           */
+          animation-mixer="clip: *"
         ></a-gltf-model>
       </a-entity>
     </a-scene>
